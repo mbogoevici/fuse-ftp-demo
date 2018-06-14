80ee455c633264b9b7f956648965af918fb3a91d
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2018-06-14 13:57:22 -0400

    Separate incoming and outgoing FTP configurations

--- src/main/java/com/redhat/fuse/mft/ftp/mftftpdemo/MftFtpDemoApplication.java
+++ src/main/java/com/redhat/fuse/mft/ftp/mftftpdemo/MftFtpDemoApplication.java
@@ -20,8 +20,8 @@ public class MftFtpDemoApplication {
             public void configure() throws Exception {
                 // monitor the FTP server and copy files downstream
                 // emit a message when a file has been received
-                from("ftp://{{mft.ftp.user}}@{{mft.ftp.host}}/{{mft.ftp.inputPath}}?password={{mft.ftp.password}}&idempotent=true")
-                        .to("{{mft.localPath}}/inbox")
+                from("ftp://{{mft.ftp.incoming.user}}@{{mft.ftp.incoming.host}}/{{mft.ftp.incoming.path}}?password={{mft.ftp.incoming.password}}&idempotent=true")
+                        .to("{{mft.local.incoming.path}}")
                         .transform().simple("${headers['CamelFileNameProduced']}")
                         .to("direct:incomingFiles");
 
@@ -31,13 +31,13 @@ public class MftFtpDemoApplication {
                         .to("direct:outgoingFiles");
 
                 // Also send files for upload when a file is copied in the 'outbox' folder locally
-                from("{{mft.localPath}}/outbox")
+                from("{{mft.local.outgoing.path}}")
                         .to("direct:outgoingFiles");
 
                 // Route for uploading to FTP
                 // The message body contains the path to the file being uploaded
                 from("direct:outgoingFiles")
-                        .to("ftp://{{mft.ftp.user}}@{{mft.ftp.host}}/{{mft.ftp.outputPath}}?password={{mft.ftp.password}}")
+                        .to("ftp://{{mft.ftp.outgoing.user}}@{{mft.ftp.outgoing.host}}/{{mft.ftp.outgoing.path}}?password={{mft.ftp.outgoing.password}}")
                         .log("Sent ${headers['CamelFileName']}");
 
             }
